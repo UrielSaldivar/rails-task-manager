a7c605e6c8c27016a5a64fc1f506d57120f877bf
Uriel Saldivar <uriel.profesional@gmail.com>
2026-05-03 19:47:35 -0600

    modificado

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "/tasks", to: "tasks#index"
-  get "/tasks:id", to: "tasks#show", ask: :task
+  get "/tasks:id", to: "tasks#show", as: :task
   get "/tasks/new", to: "tasks#new", as: :new_task
   post "/tasks", to: "tasks#create"
   get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task
